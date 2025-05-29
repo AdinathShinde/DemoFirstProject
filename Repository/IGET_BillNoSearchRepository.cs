e3ab5f9e52a1234fdf83f1737102e1286dbb8377
Adinath Shinde <adinathshinde54@gmail.com>
2025-05-29 15:02:46 +0530

    new changes

--- Repository/IGET_BillNoSearchRepository.cs
+++ Repository/IGET_BillNoSearchRepository.cs
@@ -4,7 +4,7 @@ namespace DemoFirstProject.Repository
 {
     public interface IGET_BillNoSearchRepository
     {
-      Task<List<GET_BillNoSearch>> getasync(GET_BillNoSearchDomain input);
-      Task<List<GET_BillNoSearch>> Searchasync(GET_BillNoSearchDomain input);
+        Task<List<GET_BillNoSearch>> getasync(GET_BillNoSearchDomain input);
+        Task<List<GET_BillNoSearch>> Searchasync(GET_BillNoSearchDomain input);
     }
 }
